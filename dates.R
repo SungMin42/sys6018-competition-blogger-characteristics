7fc3c1f8e03db02e9c691647856dffd70c5a98a0
Andrew Evans <andyevans8706@gmail.com>
2018-10-11 10:35:52 -0400

    Update dates.R
    updating with code from final script within textMining.R

--- dates.R
+++ dates.R
@@ -10,40 +10,29 @@ library(stringr)
 library(tidyr)
 library(lubridate)
 
-# a file of months translated to english 
-months <- as.data.frame(read.delim('months.txt', sep = ",", header = FALSE))
-colnames(months) <- c("V1","V2")
-
-# import the train and test data, removing the age (response) and binding the rows
-blogs <- read_csv('train.csv')
-test <- read_csv('test.csv')
-age <- blogs$age
-blogs <- blogs[-8]
-both <- rbind(blogs,test)
+##### Format the dates #####
 
 # string split the date column, add a row Index, and fix empty Months and Years
-dates <- str_split_fixed(both$date, ",", n = 3)
-dates <- as.data.frame(dates)
-dates <- cbind(dates,c(1:681284))
+dates <- as.data.frame(str_split_fixed(df.train.test$date, ",", n = 3))
+dates <- cbind(dates,c(1:nrow(dates)))
 dates$V2 <- as.character(dates$V2)
-colnames(dates) <- c("Day","Month","Year","Index")
-dates$Month[which(dates$Month == "")] <- "Unknown"
-dates$Day[which(dates$Day == "")] <- "Unknown"
-dates$Year[which(dates$Year == "")] <- "2005"
+colnames(dates) <- c("day","month","year","i")
+dates$month[which(dates$month == "")] <- "Unknown"
+dates$year[which(dates$year == "")] <- "2005"
 
 # Create a data frame of months that need translating
-mon <- as.data.frame(cbind(as.character(dates$Month), dates$Index))
-colnames(mon) <- c("V1","Index")
+mon <- data.frame(x = as.character(dates$month), y = as.integer(dates$i))
+colnames(mon) <- c("V1","i")
 
 # Merge the translated months and re-sort by the Index
-mon <- merge(x = mon, y = months, by.x = "V1", by.y = "V1")
-mon$Index <- as.numeric(mon$Index)
-mon <- mon[order(mon$Index),]
+library(plyr)
+mon <- join(mon, months, by = "V1", type = "left")
+mon <- mon[order(mon$i),]
 
 # Put the translated months back into the combined set
-dates$Month2 <- mon$V2
-dates$Date <- paste(dates$Year,dates$Month2,dates$Day, sep = "-")
+dates$month <- mon$V2
+dates$Date <- paste(dates$year,dates$month,dates$day, sep = "-")
 dates$Date2 <- as.Date(ymd(dates$Date))
-both$date <- dates$Date2
+df.train.test$date <- dates$Date2
 
 
